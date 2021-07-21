c066edb6c8fcc5a32469a3d03bb863d1dd752dc6
du-dim <dim-dubovik@yandex.ru>
2021-07-22 01:44:36 +0300

    init: min_max_avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return !arr || arr.length === 0 ? 0 : arr.reduce((a, b) => a < b ? a : b); 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  return !arr || arr.length === 0 ? 0 : arr.reduce((a, b) => a > b ? a : b);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  return !arr || arr.length === 0 ? 0 : arr.reduce((a, b) => a + b) / arr.length;
 }
+
